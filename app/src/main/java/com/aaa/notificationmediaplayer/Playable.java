c472a24c134d40e2b6cd165193a341f272c18f3e
Sasha <sashaagaev@gmail.com>
2020-02-26 00:14:50 +0200

    Initial commit

---
+++ app/src/main/java/com/aaa/notificationmediaplayer/Playable.java
@@ -0,0 +1,10 @@
+package com.aaa.notificationmediaplayer;
+
+public interface Playable {
+
+    void  onTrackPrevious();
+    void  onTrackPlay();
+    void  onTrackPause();
+    void  onTrackNext();
+
+}
